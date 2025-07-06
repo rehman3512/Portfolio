9cd07c4f885b83bdf63b96e00cab713ebafabffd
REHMAN KHAN <rehmankh2256@gmail.com>
2025-07-06 23:32:37 +0500

    3

--- lib/Controller/Widgets/smallButton/smallButton.dart
+++ lib/Controller/Widgets/smallButton/smallButton.dart
@@ -26,10 +26,10 @@ import 'package:portfolio/Controller/Widgets/AppColor/appColor.dart';
 class Smallbutton extends StatelessWidget {
   final Widget? child;
   const Smallbutton({super.key,required this.child});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return InkWell( onTap: (){},
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell( onTap: (){},
 //       child: Container(height: 45,width: 45,
 //           decoration: BoxDecoration(
 //               color: AppColor.greyColor,
