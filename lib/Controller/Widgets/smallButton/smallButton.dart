07d61ed9355f41dfbd68047d98b13719f5ad68d8
REHMAN KHAN <rehmankh2256@gmail.com>
2025-07-06 23:32:00 +0500

    1

--- lib/Controller/Widgets/smallButton/smallButton.dart
+++ lib/Controller/Widgets/smallButton/smallButton.dart
@@ -1,22 +1,44 @@
+// import 'package:flutter/material.dart';
+// import 'package:portfolio/Controller/Widgets/AppColor/appColor.dart';
+//
+// class Smallbutton extends StatelessWidget {
+//   final Widget? child;
+//   const Smallbutton({super.key,required this.child});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell( onTap: (){},
+//       child: Container(height: 45,width: 45,
+//           decoration: BoxDecoration(
+//               color: AppColor.greyColor,
+//               borderRadius: BorderRadius.circular(6),
+//               border: Border.all(color: AppColor.greyColor)
+//           ),child: Center(
+//             child: child
+//           )
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:portfolio/Controller/Widgets/AppColor/appColor.dart';
 
-class Smallbutton extends StatelessWidget {
-  final Widget? child;
-  const Smallbutton({super.key,required this.child});
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell( onTap: (){},
-      child: Container(height: 45,width: 45,
-          decoration: BoxDecoration(
-              color: AppColor.greyColor,
-              borderRadius: BorderRadius.circular(6),
-              border: Border.all(color: AppColor.greyColor)
-          ),child: Center(
-            child: child
-          )
-      ),
-    );
-  }
-}
+// class Smallbutton extends StatelessWidget {
+//   final Widget? child;
+//   const Smallbutton({super.key,required this.child});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell( onTap: (){},
+//       child: Container(height: 45,width: 45,
+//           decoration: BoxDecoration(
+//               color: AppColor.greyColor,
+//               borderRadius: BorderRadius.circular(6),
+//               border: Border.all(color: AppColor.greyColor)
+//           ),child: Center(
+//               child: child
+//           )
+//       ),
+//     );
+//   }
+// }
