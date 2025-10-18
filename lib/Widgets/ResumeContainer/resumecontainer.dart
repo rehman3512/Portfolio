74f5ee04fe8341476ba6894c13b1146fd350a30a
REHMAN KHAN <rehmankh2256@gmail.com>
2025-10-18 12:44:45 -0700

    update

--- lib/Widgets/ResumeContainer/resumecontainer.dart
+++ lib/Widgets/ResumeContainer/resumecontainer.dart
@@ -8,8 +8,8 @@ class ResumeContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      width: 400,
+      height: MediaQuery.of(context).size.height * 0.15,
+      width: MediaQuery.of(context).size.width * 0.50,
       decoration: BoxDecoration(
         color: AppColor.lightGreyColor,
         border: Border.all(
