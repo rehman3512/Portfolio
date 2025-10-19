5d035de5c7e52eaf3a3d6ac49fd5d2eec433fd74
REHMAN KHAN <rehmankh2256@gmail.com>
2025-10-19 11:49:43 -0700

    4

--- lib/Widgets/ResumeContainer/resumecontainer.dart
+++ lib/Widgets/ResumeContainer/resumecontainer.dart
@@ -9,6 +9,8 @@ class ResumeContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       // height: MediaQuery.of(context).size.height * 0.25,
+      // width: MediaQuery.of(context).size.width * 0.50,
+      height: 200,
       width: 400,
       decoration: BoxDecoration(
         color: AppColor.lightGreyColor,
