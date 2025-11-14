af0ce4c55c2de9034ae59ab26f397ae4157e45d4
REHMAN KHAN <rehmankh2256@gmail.com>
2025-11-14 05:07:24 -0800

    Update appColor.dart

--- lib/Constants/AppColor/appColor.dart
+++ lib/Constants/AppColor/appColor.dart
@@ -6,4 +6,17 @@ class AppColor{
   static Color greyColor = Color(0xff333333);
   static Color lightGreyColor = Color(0xff444444);
   static Color redColor = Color(0xffFF0000);
+
+  static Color whiteColor = Color(0xffFFFFFF);
+  static Color blackColor = Color(0xff000000);
+  static Color transparentColor = Color(0x00000000);
+  // static Color turquoiseColor = Color(0xff49BBBD);
+  // static final Color lightTurquoiseColor = Color(0xFFE0F7F7);
+  // static Color redColor = Color(0xffFF0000);
+  // static Color purpleColor = Color(0xFF800080);
+  // static final Color lightPurpleColor = Color(0xFFD8BFD8);
+  // static Color greenColor = Color(0xFF008000);
+  // static Color greyColor = Color(0xFFA9A9A9);
+  // static Color yellowColor = Color(0xFFFFD700);
+
 }
